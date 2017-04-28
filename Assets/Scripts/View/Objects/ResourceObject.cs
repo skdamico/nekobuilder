2f8b446dc6c6a3e19f1e3bd36641b43e4c24313b
Stefano DAmico <skdamico@gmail.com>
2017-04-27 19:57:38 -0700

    Use materials for the resources

--- Assets/Scripts/View/Objects/ResourceObject.cs
+++ Assets/Scripts/View/Objects/ResourceObject.cs
@@ -14,6 +14,34 @@ public class ResourceObject {
 
         view = Resources.Load<GameObject>("Prefabs/ResourcePrefab");
 
-        MonoBehaviour.Instantiate(view, position, Quaternion.identity, parent.transform);
+        view = MonoBehaviour.Instantiate(view, position, Quaternion.identity, parent.transform);
+
+        SetupView();
+    }
+
+
+    public void SetupView() {
+        view.SetActive(resourceData.Life > 0);
+
+        // Set color of mesh based on object type
+        MeshRenderer renderer = view.GetComponent<MeshRenderer>(); 
+
+        Material material;
+        switch(resourceData.Kind) {
+            case (Resource.ResourceKind.Build):
+                material = Resources.Load<Material>("Material/ResourceBuildMaterial");
+                break;
+            case (Resource.ResourceKind.Fuel):
+                material = Resources.Load<Material>("Material/ResourceFuelMaterial");
+                break;
+            case (Resource.ResourceKind.Science):
+                material = Resources.Load<Material>("Material/ResourceScienceMaterial");
+                break;
+            default:
+                material = Resources.Load<Material>("Material/ErrorMaterial");
+                Debug.LogError("Invalid data for Resource.Kind");
+                break;
+        }
+        renderer.material = material;
     }
 }
